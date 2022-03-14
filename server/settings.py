8d7af60c7ff46b9e0e98aaeb354beaf715631f5a
ritikgupta21092000 <guptaritik349@gmail.com>
2022-03-14 16:05:13 +0530

    Allowed all host

--- server/settings.py
+++ server/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['baglocater-server.herokuapp.com', '0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
